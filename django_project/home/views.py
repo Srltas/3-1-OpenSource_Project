03c980ca89b0822f61abab1f9f7924da1702a0b9
Dong_Min Kim <kdm97912@gmail.com>
2020-06-10 23:16:51 +0900

    회원가입 기능 개발 - 사용자 이름, 사용자 이메일, 비밀번호, 비밀번호 확인 총 4개의 데이터를 입력 받고 모든 문자열을 입력하지 않을 시, 비밀번호 입력 두번을 다르게 입력 시 에러 표시

--- django_project/home/views.py
+++ django_project/home/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    return render(request,'index.html')
 
 
 # Create your views here.
