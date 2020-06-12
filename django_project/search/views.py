3dcb8abff245b5a52aeff1a114b942985b4dae48
Dong_Min Kim <kdm97912@gmail.com>
2020-06-12 18:45:11 +0900

    6_12

--- django_project/search/views.py
+++ django_project/search/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+
+def search(request):
+
     return render(request, 'search/search_result.html')
+
+
