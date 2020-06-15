0f4c4b56c898cd06db5b66c06abb269b74186253
Dong_Min Kim <kdm97912@gmail.com>
2020-06-16 01:40:09 +0900

    search_result 데이터 기본 저장 툴 생성 / 페이징 기능 개발

--- django_project/search/views.py
+++ django_project/search/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     kw = {} #임시로 값이 잘 들어갔나 확인용
     kw['kw'] = request.GET.get('kw', '')
-    return render(request, 'search/search_result.html',kw)
+
+    # 입력 파라미터
+    page = request.GET.get('page', '1') # 페이지
+
+    # 조회
+    page_list = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+
+    # 페이징처리
+    paginator = Paginator(page_list,10)
+    page_obj = paginator.get_page(page)
+
+    context = {'book_list': page_obj}
+    return render(request, 'search/search_result.html', context)
 
 
