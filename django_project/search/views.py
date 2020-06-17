c565f96bbbee3f0c798fb5cbbc4bf5341430bdbd
hong <hkp1030@gmail.com>
2020-06-17 14:17:05 +0900

    검색 결과 페이징 처리

--- django_project/search/views.py
+++ django_project/search/views.py
@@ -7,10 +7,10 @@ import json
 def index(request):
     # 입력 파라미터
     kw = request.GET.get('kw')
-    page = request.GET.get('page', 1) # 페이지
+    page = int(request.GET.get('page', '1')) # 페이지
 
     if kw == '':
-        page_list = {'error' : 1}
+        page_list = {'error' : True}
     else:
         # 조회
         page_list = lib_search(kw, page)
@@ -29,10 +29,18 @@ title = 도서명
 page = 요청 페이지
 
 반환 리스트 :
+'keyword' = 검색한 단어
 'lastBuildDate' = 요청 날짜
 'total' = 검색된 총 책 수
+'number' = 현재 페이지 번호
+'previousPageNumber' = 이전 페이지 번호
+'nextPageNumber' = 다음 페이지 번호
+'hasPrevious' = 이전 페이지 존재여부
+'hasNext' = 다음 페이지 존재여부
 'lastPage' = 총 페이지 수
 'items' = 검색된 책의 리스트
+'pageList' = 페이지 리스트
+
 
 'items' 책의 속성 : 
 'title' = 책 제목
@@ -68,15 +76,45 @@ def lib_search(title, page=1):
         json_data = json.loads(response_body.decode('utf-8'))
         # book_list = json_data["items"]
 
-        # 마지막 페이지 추가
+        # 책 리스트가 비어있으면 오류 반환
+        if not json_data['items']:
+            return {'error' : True}
+
+        # 검색한 단어 추가
+        json_data['keyword'] = title
+
+        # 이전 페이지 존재 여부, 다음 페이지 존재 여부 추가
+        json_data['hasPrevious'] = True
+        json_data['hasNext'] = True
+
+        # 총 페이지 수 추가
         if json_data['total'] > 1000:
             json_data['lastPage'] = 100
         else:
             json_data['lastPage'] = int(json_data['total'] / 10 + 1)
 
         # 페이지 리스트 추가
-        pageP = int(page / 10)
-        json_data['pageList'] = [i for i in range(pageP+1, pageP+11)]
+        if page % 10 != 0:
+            pageStart = page - (page % 10) + 1
+            pageEnd = page - (page % 10) + 10
+        else:
+            pageStart = page - 9
+            pageEnd = page
+        if pageEnd < json_data['lastPage']:
+            json_data['pageList'] = [i for i in range(pageStart, pageEnd+1)]
+        else:
+            json_data['pageList'] = [i for i in range(pageStart, json_data['lastPage']+1)]
+            json_data['hasNext'] = False
+
+        if pageStart == 1:
+            json_data['hasPrevious'] = False
+
+        # 다음 페이지 번호, 이전 페이지 번호 추가
+        json_data['previousPageNumber'] = pageStart - 1
+        json_data['nextPageNumber'] = pageEnd + 1
+
+        # 현재페이지 번호 추가
+        json_data['number'] = page
 
         print(json_data)
         return json_data
