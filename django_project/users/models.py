41cf6bade6115a153413beb6407fe7174408be3f
Dong_Min Kim <kdm97912@gmail.com>
2020-06-17 16:23:01 +0900

    초기 테스트용 DB 삭제

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -9,20 +9,6 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
-class users(models.Model):
-    username = models.CharField(max_length=64, verbose_name='사용자명')
-    useremail = models.EmailField(max_length=128, verbose_name='사용자이메일')
-    password = models.CharField(max_length=64, verbose_name='비밀번호')
-    registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
-
-    def __str__(self):
-        return self.username
-
-    class Meta:
-        db_table = 'library_users'
-        verbose_name_plural = '도서관 사용자'
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField('아이디', max_length=30, unique=True)
     email = models.EmailField('이메일', blank=True)
