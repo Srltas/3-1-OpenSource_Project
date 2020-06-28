3dc2d74acbc16b774228fb4043dd42f965e6a438
hong <fya1030@naver.com>
2020-06-28 23:48:02 +0900

    개인 맞춤형 도서 추천 함수 recommendedBooks 추가

--- django_project/home/views.py
+++ django_project/home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from users.views import recommendedBooks
 
 
 def index(request):
-    return render(request,'home/index.html')
+    content = {}
+    if request.user.is_authenticated:
+        content['rcmBooks'] = recommendedBooks(request.user)
+
+    print(content)
+
+    return render(request,'home/index.html', content)
 
 
 # Create your views here.
